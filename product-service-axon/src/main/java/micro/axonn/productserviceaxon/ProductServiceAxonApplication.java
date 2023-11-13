115870903710380af8989d7271a38306667a0405
soulaiman ghailan <soulaiman8ghailan@gmail.com>
2023-11-14 00:13:22 +0100

    f com

---
+++ product-service-axon/src/main/java/micro/axonn/productserviceaxon/ProductServiceAxonApplication.java
@@ -0,0 +1,13 @@
+package micro.axonn.productserviceaxon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceAxonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceAxonApplication.class, args);
+    }
+
+}
---
+++ product-service-axon/src/test/java/micro/axonn/productserviceaxon/ProductServiceAxonApplicationTests.java
@@ -0,0 +1,13 @@
+package micro.axonn.productserviceaxon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceAxonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
